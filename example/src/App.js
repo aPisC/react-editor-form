0b09a13a4d98fc751425e3b1bb509099d0fd6e59
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-06-08 17:29:59 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-editor-form'
+import 'react-editor-form/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
