ade8dcd3b8418a2ec693c067085ab2ae6ec30812
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-06-09 18:31:46 +0200

    Add useEditorInitializer hook

---
+++ src/useEditorInitializer.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useContext, useState } from 'react'
+import EditorEnvironment from './EditorEnvironment'
+
+const useEditorInitializer = (initializer, initialValue=null, dependencies) => {
+  const editor = useContext(EditorEnvironment.Context);
+  const [state, setState] = useState(initialValue);
+
+  const fetchData = () => {
+    editor.trackPromise(initializer(editor.id).then((x) => (setState(x))))
+  };
+  useEffect(fetchData, dependencies || []);
+
+  return [state, fetchData];
+}
+
+export default useEditorInitializer
