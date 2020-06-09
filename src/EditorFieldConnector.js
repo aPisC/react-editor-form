66d9bcd0f9e5ba9ca771acb428a7f39c999db290
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-06-09 18:31:10 +0200

    add children to EditorFieldConnector

--- src/EditorFieldConnector.js
+++ src/EditorFieldConnector.js
@@ -2,16 +2,16 @@ import React, { useContext } from 'react'
 import EditorEnvironment from './EditorEnvironment'
 import AccessMember from './Converters/AccessMember'
 
-const InnerFieldComponent = ({component, valuePropName, valueLoader, ...props}) => {
+const InnerFieldComponent = ({component, valuePropName, valueLoader, children, ...props}) => {
   const editor = useContext(EditorEnvironment.Context)
   return React.createElement(component, {
     ...props,
     onChange: (event) => editor.update((valueLoader || (event=>event.target.value))(event)),
     [valuePropName || "value"]: editor.data == null ? '' : editor.data
-  })
+  }, children )
 }
 
-function EditorFieldConnector({ component, valuePropName, valueLoader, member, ...props }) {
+function EditorFieldConnector({ component, valuePropName, valueLoader, member, ...props}) {
   if (member) return <AccessMember member={member}>
     <InnerFieldComponent component={component} {...props} valuePropName={valuePropName} valueLoader={valueLoader} />
   </AccessMember>;
