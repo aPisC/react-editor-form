ed772240ceb997243d25c3765ead8af01f4d5db0
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-06-09 18:31:30 +0200

    Add EditorInitializer component

---
+++ src/EditorInitializer.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useContext, useState } from 'react'
+import EditorEnvironment from './EditorEnvironment'
+
+const EditorInitializer = ({initializer, onUpdate, dependencies}) => {
+  const editor = useContext(EditorEnvironment.Context);
+
+  const fetchData = () => {
+    editor.trackPromise(initializer(editor.id).then((x) => (onUpdate(x))))
+  };
+  useEffect(fetchData, dependencies || []);
+
+  return null;
+}
+
+export default EditorInitializer
