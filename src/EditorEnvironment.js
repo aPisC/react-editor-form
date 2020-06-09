e82426db26ace6a5811b028d3e63f2505683cad3
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-06-09 18:30:35 +0200

    Add emitter, promie tracker to editor environment

--- src/EditorEnvironment.js
+++ src/EditorEnvironment.js
@@ -1,38 +1,139 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const EditorContext = React.createContext({})
 
-function EditorEnvironment({ id, persistance, children }) {
-  const [data, setData] = useState(null);
+function EditorEnvironment({ id, persistance, children, emitHandler }) {
+  const [data, setData] = useState(null)
   const [currentPromise, setCurrentPromise] = useState(null)
 
-  const update = (d) => { setData(d) }
-  const save = () => {
-    if(id == null)
-      persistance.create(id, data);
-    else
-      persistance.update(id, data)
+  // Guard for updating mounted components only
+  const isMountedRef = useRef(false)
+  useEffect(() => {
+    isMountedRef.current = true
+    return () => {
+      isMountedRef.current = true
     }
-  const del = () => {}
+  }, [])
 
-  const trackPromise = (p) => {
-    const mp = currentPromise == null ? p : Promise.all([currentPromise, p]);
-    setCurrentPromise(mp)
-    mp.then(()=> setCurrentPromise(cp => {
-      if(cp == mp) return null;
-      return cp;
-    }))
-  } 
-
-  useEffect(()=> {
-    trackPromise(
-    persistance
-      .load(id)
-      .then(data => {
-        console.log("loaded", data)
-        setData(data);
+  /****************************
+   * Signal emitting function
+   */
+  // emitting a signal, returns true, if it was not canceled
+  const emit = (emitData, isCancelable = false) => {
+    let isCanceled = false
+    if (emitHandler) {
+      emitHandler({
+        ...(isCancelable
+          ? {
+              cancel: () => {
+                isCanceled = true
+              }
+            }
+          : {}),
+        data,
+        id,
+        ...emitData
       })
-    )
+    }
+    return !isCanceled
+  }
+  /*****************************
+   * Promise tracking functions
+   *
+   * used for setting isLoading value to false
+   * when there is no pending promise for the environment
+   */
+  const clearTrackedPromise = (p) => {
+    if (isMountedRef.current)
+      setCurrentPromise((currentPromise) => {
+        if (currentPromise == p) return null
+        return currentPromise
+      })
+  }
+  const trackPromise = (p) => {
+    if (isMountedRef.current)
+      setCurrentPromise((currentPromise) => {
+        const mp = currentPromise == null ? p : Promise.all([currentPromise, p])
+        mp.then(() => {
+          clearTrackedPromise(mp)
+        }).catch(() => {
+          clearTrackedPromise(mp)
+        })
+        return mp
+      })
+  }
+
+  const update = (d) => {
+    if (emit({ type: 'BEFORE_UPDATE', data: d, oldData: data }, true)) {
+      setData(d)
+      emit({ type: 'AFTER_UPDATE' })
+    }
+  }
+
+  /************************************
+   * Basic data store handler functions
+   * (load, save, delete)
+   */
+  const save = () => {
+    if (emit({ type: 'BEFORE_SAVE' }, true)) {
+      const p = (id == null
+        ? persistance.create(data)
+        : persistance.update(id, data)
+      )
+        .then((r) => {
+          emit({ type: 'AFTER_SAVE' })
+          return r
+        })
+        .catch((error) => {
+          emit({ type: 'ERROR_SAVE', error })
+          throw error
+        })
+      trackPromise(p)
+      return p
+    }
+  }
+
+  // deletes current object (using persistance interface)
+  const del = () => {
+    if (emit({ type: 'BEFORE_DELETE' }, true)) {
+      const p = persistance
+        .delete(id, data)
+        .then((r) => {
+          emit({ type: 'AFTER_DELETE' })
+          return r
+        })
+        .catch((error) => {
+          emit({ type: 'ERROR_DELETE', error })
+          throw error
+        })
+      trackPromise(p)
+      return p
+    }
+  }
+
+  // load data by the given id (using persistance interface)
+  const load = () => {
+    const p = persistance
+      .load(id)
+      .then((data) => {
+        setData(data)
+        return data
+      })
+      .then((data) => {
+        emit({ type: 'AFTER_LOAD', data })
+        return data
+      })
+      .catch((error) => {
+        emit({ type: 'ERROR_LOAD', error })
+        throw error
+      })
+    trackPromise(p)
+    return p
+  }
+
+  // load data on editor initialization
+  useEffect(() => {
+    load()
   }, [id])
 
   const editorData = {
@@ -41,16 +142,18 @@ function EditorEnvironment({ id, persistance, children }) {
     isLoading: currentPromise != null,
     update,
     save,
+    load,
     delete: del,
     trackPromise,
+    emit
   }
 
   return <EditorContext.Provider children={children} value={editorData} />
 }
 
-EditorEnvironment.Context = EditorContext;
-EditorEnvironment.Consumer = function ({children}) {
-  return <EditorContext.Consumer children={children}/>
+EditorEnvironment.Context = EditorContext
+EditorEnvironment.Consumer = function ({ children }) {
+  return <EditorContext.Consumer children={children} />
 }
 
-export default EditorEnvironment;
+export default EditorEnvironment
