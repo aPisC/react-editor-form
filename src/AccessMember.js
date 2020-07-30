b4ad6bdf6427d6b35841bf9cea8c4033aa256d9a
Bendegúz Cseresnyés <cser.bende@gmail.com>
2020-07-30 10:39:47 +0200

    Add support for nested objects in accessmember

--- src/AccessMember.js
+++ src/AccessMember.js
@@ -2,11 +2,24 @@ import React from 'react'
 import ValueConverter from './ValueConverter'
 
 export default function AccessMember(props) {
-  return (
+  const seg = props.member.split('.')
+  const last = seg[seg - 1]
+  let am = (
     <ValueConverter
-      load={(d) => d && d[props.member]}
-      update={(v, o) => o && { ...o, [props.member]: v }}
+      load={(d) => d && d[last]}
+      update={(v, o) => o && { ...o, [last]: v }}
       children={props.children}
     />
   )
+  for (let i = seg.length - 2; i >= 0; i--) {
+    const part = seg[i]
+    am = (
+      <ValueConverter
+        load={(d) => d && d[part]}
+        update={(v, o) => o && { ...o, [part]: v }}
+        children={am}
+      />
+    )
+  }
+  return am
 }
